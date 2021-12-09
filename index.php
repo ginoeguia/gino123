cdd5e1cb0f4ccd2188b822a068a844294880d063
ginoeguia <75793744+ginoeguia@users.noreply.github.com>
2021-12-08 20:26:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,68 @@
+<?php
+include_once 'crud.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo CRUD</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <br>
+    <br>
+    <div id="form" >
+        <form method="post">
+            <table width="100%" border="1" cellpadding="15">
+                <tr>
+                    <td>
+                        <input type="text" name="fn" value="<?php if(isset($_GET['edit'])) echo $getROW['fn']; ?>"placeholder="Nombre">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="ln" value="<?php if(isset($_GET['edit'])) echo $getROW['ln']; ?>"placeholder="Apellido">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <?php
+                        if(isset($_GET['edit'])){
+                            ?>
+                            <button type="submit" name="update">Editar</button>
+                            <?php
+                        }else{
+                            ?>
+                            <button type="submit" name="save">Registrar</button>
+                            <?php
+                        }
+                        ?>
+                    </td>
+                </tr>
+            </table>
+        </form>
+        <br><br>
+        <table width="100%" border="1" cellpadding="15" aling="center">
+            <?php
+                $res = $MySQLiconn->query("SELECT * FROM data");
+                while ($row=$res->fetch_array()){
+                  ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['fn']; ?></td>
+                        <td><?php echo $row['ln']; ?></td>
+                        <td><a href="?edit=<?php echo $row['id'];?>"
+                            onclick="return confirm('Confirmar edicion')">Editar</a></td>
+                        <td><a href="?del=<?php echo $row['id'];?>"
+                            onclick="return confirm('Confirmar eliminacion')">Eliminar</a></td>
+                        
+                    </tr> 
+                  <?php   
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
